0e1842cf4426ba98d492309616ec9149d51c6831
Lewis Thorley <lewis.thorley@netlify.com>
2023-12-12 07:10:48 +0000

    Get all site info for current dir

--- lua/netlify/init.lua
+++ lua/netlify/init.lua
@@ -3,13 +3,57 @@ local M = {}
 function M.show_netlify_dashboard()
 	local buf = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Welcome to Netlify Dashboard" })
-	local get_sites_command = "netlify sites:list --json"
+	local get_status_command = "netlify status --json"
+
 	-- Execute the command and capture the output
-	local sites = vim.fn.system(get_sites_command)
+	local statusResponse = vim.fn.system(get_status_command)
+
+	local statusJson = vim.fn.json_decode(statusResponse)
+
+	if statusJson == nil then
+		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "No Netlify site found" })
+		return
+	end
+	local siteId = statusJson["siteId"]
+
+	local siteResponse = vim.fn.system("netlify api getSite --data '{\"siteId\": \"" .. siteId .. "\"}'")
+
+	local siteJson = vim.fn.json_decode(siteResponse)
+
+	if siteJson == nil then
+		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "No Netlify site found" })
+		return
+	end
+
+	local siteName = siteJson["name"]
+	local siteUrl = siteJson["url"]
+	local siteAdminUrl = siteJson["adminUrl"]
+
+	local status = "Site: " .. siteName .. "\n"
+	status = status .. "URL: " .. siteUrl .. "\n"
+	status = status .. "Admin URL: " .. siteAdminUrl .. "\n"
+
+	local deployResponse = vim.fn.system("netlify api listSiteDeploys --data '{\"siteId\": \"" .. siteId .. "\"}'")
+
+	local deployJson = vim.fn.json_decode(deployResponse)
+
+	if deployJson == nil then
+		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "No Netlify site found" })
+		return
+	end
+
+	local deployUrl = deployJson[1]["deploy_ssl_url"]
+	local deployAdminUrl = deployJson[1]["admin_url"]
+	local deployState = deployJson[1]["state"]
+
+	status = status .. "Deploy: " .. deployState .. "\n"
+	status = status .. "URL: " .. deployUrl .. "\n"
+	status = status .. "Admin URL: " .. deployAdminUrl .. "\n"
+
 
 
 	-- Set the lines of the buffer to the output of the command
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(sites, "\n"))
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(status, "\n"))
 	local win = vim.api.nvim_open_win(buf, true, {
 		relative = "editor",
 		width = 40,
